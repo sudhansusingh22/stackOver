c8dcb3084c0e7490d2bb4f42c64c7d4539445aa4
sudhansusingh22 <sudhansusingh22@gmail.com>
2016-05-16 15:34:02 -0400

    extract title, url fro stackoverflow newest questions

--- stackOver/items.py
+++ stackOver/items.py
@@ -5,10 +5,11 @@
 # See documentation in:
 # http://doc.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy.item import Item, Field
 
 
-class StackoverItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
+class StackItem(Item):
+    title = Field()
+    url = Field()
+
     pass
---
+++ stackOver/spiders/stack_spider.py
@@ -0,0 +1,20 @@
+from scrapy import Spider
+from scrapy.selector import Selector
+from stackOver.items import StackItem
+
+
+class StackSpider(Spider):
+    name = "stack"
+    allowed_domains = ["stackoverflow.com"]
+    start_urls = ["http://stackoverflow.com/questions?sort=newest"]
+
+    def parse(self, response):
+        questions = Selector(response).xpath('//div[@class="summary"]/h3')
+
+        for question in questions:
+            item = StackItem()
+            item['title'] = question.xpath(
+                'a[@class="question-hyperlink"]/text()').extract()[0]
+            item['url'] = question.xpath(
+                'a[@class="question-hyperlink"]/@href').extract()[0]
+            yield item
