5344b0fa8233a175bf51c0a2e9df28fa0ea33146
sudhansusingh22 <sudhansusingh22@gmail.com>
2016-05-16 21:27:38 -0400

    added comments

--- stackOver/pipelines.py
+++ stackOver/pipelines.py
@@ -13,20 +13,24 @@ from scrapy import log
 class StackoverPipeline(object):
     
     def __init__(self):
+        # create mongoDB connection
         connection = pymongo.MongoClient(
             settings['MONGODB_SERVER'],
             settings['MONGODB_PORT']
         )
+        # connect to database
         db = connection[settings['MONGODB_DB']]
         self.collection = db[settings['MONGODB_COLLECTION']]
 
     def process_item(self, item, spider):
         valid = True
+        # check if data is present or not
         for data in item:
             if not data:
                 valid = False
                 raise DropItem("Missing {0}!".format(data))
         if valid:
+            # insert data to MongoDB collection
             self.collection.insert(dict(item))
             log.msg("Question added to MongoDB database!",
                     level=log.DEBUG, spider=spider)
