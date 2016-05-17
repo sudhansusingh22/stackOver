d4cd9ff9d2c92b102d73e0dd20113dd3b2a4a42c
sudhansusingh22 <sudhansusingh22@gmail.com>
2016-05-16 21:18:03 -0400

    added settings for Mongodb DB and collection

--- stackOver/settings.py
+++ stackOver/settings.py
@@ -64,10 +64,16 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'stackOver.pipelines.SomePipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'stackOver.pipelines.StackoverPipeline': 300,
+}
 
+# ITEM_PIPELINES = ['stack.pipelines.MongoDBPipeline', ]
+
+MONGODB_SERVER = "localhost"
+MONGODB_PORT = 27017
+MONGODB_DB = "stackoverflow"
+MONGODB_COLLECTION = "questions"
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
 #AUTOTHROTTLE_ENABLED = True
