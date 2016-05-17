f65a917e0174a914558bffb7189c5605060cefb1
sudhansusingh22 <sudhansusingh22@gmail.com>
2016-05-16 21:18:57 -0400

    spide will extract userdata: username and password as well

--- stackOver/spiders/stack_spider.py
+++ stackOver/spiders/stack_spider.py
@@ -10,11 +10,14 @@ class StackSpider(Spider):
 
     def parse(self, response):
         questions = Selector(response).xpath('//div[@class="summary"]/h3')
+        userDetails = Selector(response).xpath('//div[@class="summary"]//div[@class="user-details"]')
 
-        for question in questions:
+        for question,user in zip(questions,userDetails):
             item = StackItem()
             item['title'] = question.xpath(
                 'a[@class="question-hyperlink"]/text()').extract()[0]
             item['url'] = question.xpath(
                 'a[@class="question-hyperlink"]/@href').extract()[0]
+            item['userName'] = user.xpath('a/text()').extract()[0]
+            item['userUrl'] = user.xpath('a/@href').extract()[0]  
             yield item
